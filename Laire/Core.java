61b7b6056b788d2846c73587dc3e6ae190f7bb7a
Merbikoss <132165035+Merbikoss@users.noreply.github.com>
2024-09-15 15:50:27 +0300

    Update Core.java
    Updated version string.

--- Laire/Core.java
+++ Laire/Core.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Core {
     public String ProgramDone = "Program finished. Relaunch via the terminal.";
-    public String LaireVersion = "Experiment Series 1 - Stage 4";
+    public String LaireVersion = "Experimental Version 5";
     public Scanner LaireScanner = new Scanner(System.in);
     public Random LaireRandom = new Random();
 }
